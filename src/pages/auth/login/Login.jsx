05a02180659933051d39a95b0716191523d35f6c
Subroto Kumar <152253595+SubrotoKumar7@users.noreply.github.com>
2025-11-20 19:17:53 +0600

    login method added and seperate social login

--- src/pages/auth/login/Login.jsx
+++ src/pages/auth/login/Login.jsx
@@ -1,7 +1,33 @@
 import React from 'react';
 import { Link } from 'react-router';
+import useAuth from '../../../hooks/useAuth';
+import SocialLogin from '../socialLogin/SocialLogin';
+import { useForm } from 'react-hook-form';
+import Swal from 'sweetalert2';
+import toast from 'react-hot-toast';
 
 const Login = () => {
+    const {loginUser} = useAuth();
+    const {register, handleSubmit, formState: {errors}} = useForm();
+
+    const handleLogin = (data) => {
+        loginUser(data.email, data.password)
+        .then(res => {
+            if(res.user){
+                Swal.fire({
+                position: "top",
+                icon: "success",
+                title: "Login successful",
+                showConfirmButton: false,
+                timer: 1500
+                });
+            }
+        })
+        .catch(err => {
+            toast.error(err.message);
+        })
+    }
+
     return (
         <div>
             <div className='mb-5'>
@@ -9,24 +35,26 @@ const Login = () => {
                 <h3 className='font-medium'>Login with ZapShift</h3>
             </div>
             <div>
-                <form>
+                <form onSubmit={handleSubmit(handleLogin)}>
                     <fieldset className="fieldset">
+                    {/* email */}
                     <label className="label">Email</label>
-                    <input type="email" className="input w-full" placeholder="Email" />
+                    <input type="email" {...register("email", {required: true})} className="input w-full" placeholder="Email" />
+                    {errors.email?.type === 'required' && <p className='text-red-500'>Email is required</p>}
+
+                    {/* password */}
                     <label className="label">Password</label>
-                    <input type="password" className="input w-full" placeholder="Password" />
+                    <input type="password" {...register('password', {required: true, minLength: 6})} className="input w-full" placeholder="Password" />
+                    {errors.password?.type === 'required' && <p className='text-red-500'>Password is required</p>}
+                    {errors.password?.type === 'minLength' && <p className='text-red-500'>Password length should be 6 characters</p>}
+
                     <div><a className="link link-hover">Forgot password?</a></div>
                     <button className="btn btn-primary text-black font-bold mt-4">Login</button>
                     </fieldset>
                 </form>
                 <div className='my-2'>Don’t have any account? <Link className='text-secondary font-semibold underline' to={'/register'}>Register</Link></div>
                 <p className='text-center my-2'>OR</p>
-                <div>
-                    <button className="btn w-full bg-white text-black border-[#e5e5e5]">
-                        <svg aria-label="Google logo" width="16" height="16" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><g><path d="m0 0H512V512H0" fill="#fff"></path><path fill="#34a853" d="M153 292c30 82 118 95 171 60h62v48A192 192 0 0190 341"></path><path fill="#4285f4" d="m386 400a140 175 0 0053-179H260v74h102q-7 37-38 57"></path><path fill="#fbbc02" d="m90 341a208 200 0 010-171l63 49q-12 37 0 73"></path><path fill="#ea4335" d="m153 219c22-69 116-109 179-50l55-54c-78-75-230-72-297 55"></path></g></svg>
-                        Login with Google
-                    </button>
-                </div>
+                <SocialLogin></SocialLogin>
             </div>
         </div>
     );

2f444860c6d454df565874a4bc8b405e22fd4279
Subroto Kumar <152253595+SubrotoKumar7@users.noreply.github.com>
2025-11-22 00:06:12 +0600

    setup tanstack query and fetch data

--- src/pages/dashboard/myParcels/MyParcels.jsx
+++ src/pages/dashboard/myParcels/MyParcels.jsx
@@ -1,9 +1,21 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
+import useAuth from '../../../hooks/useAuth';
+import useAxiosSecure from '../../../hooks/useAxiosSecure';
 
 const MyParcels = () => {
+    const {user} = useAuth();
+    const axiosSecure = useAxiosSecure();
+    const {data : parcels = []} = useQuery({
+        queryKey: ['myParcels', user?.email],
+        queryFn: async() => {
+            const res = await axiosSecure.get(`/parcels?email=${user?.email}`);
+            return res.data;
+        }
+    })
     return (
         <div>
-            <h1>My All Parcels here</h1>
+            <h1>My All Parcels here {parcels.length}</h1>
         </div>
     );
 };

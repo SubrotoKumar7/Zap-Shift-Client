96e62fdf72eb1a866a3991c7c19930bdb6b64468
Subroto Kumar <152253595+SubrotoKumar7@users.noreply.github.com>
2025-12-01 00:17:03 +0600

    added server api

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -1,47 +1,48 @@
-import axios from 'axios';
-import React, { useEffect } from 'react';
-import useAuth from './useAuth';
-import { useNavigate } from 'react-router';
+import axios from "axios";
+import React, { useEffect } from "react";
+import useAuth from "./useAuth";
+import { useNavigate } from "react-router";
 
 const axiosSecure = axios.create({
-    baseURL: 'http://localhost:3000'
-})
+  baseURL: "https://zap-shift-server-omega-ten.vercel.app",
+});
 
 const useAxiosSecure = () => {
-    const {user, logoutUser} = useAuth();
-    const navigate = useNavigate();
+  const { user, logoutUser } = useAuth();
+  const navigate = useNavigate();
 
-    useEffect(()=> {
-        // interceptor request
-        const reqInterceptors = axiosSecure.interceptors.request.use((config)=> {
-            config.headers.Authorization = `Bearer ${user?.accessToken}`
-            return config;
-        })
+  useEffect(() => {
+    // interceptor request
+    const reqInterceptors = axiosSecure.interceptors.request.use((config) => {
+      config.headers.Authorization = `Bearer ${user?.accessToken}`;
+      return config;
+    });
 
-        // interceptor response
-        const resInterceptors = axiosSecure.interceptors.response.use((response)=> {
-            return response;
-        },
-        (err)=> {
-            console.log(err);
-            const statusCode = err.status;
-            if(statusCode === 401 || statusCode === 403){
-                logoutUser()
-                .then(()=> {
-                    navigate('/login');
-                })
-            }
-
-            return Promise.reject(err);
-        })
-
-        return () => {
-            axiosSecure.interceptors.request.eject(reqInterceptors);
-            axiosSecure.interceptors.response.eject(resInterceptors);
+    // interceptor response
+    const resInterceptors = axiosSecure.interceptors.response.use(
+      (response) => {
+        return response;
+      },
+      (err) => {
+        console.log(err);
+        const statusCode = err.status;
+        if (statusCode === 401 || statusCode === 403) {
+          logoutUser().then(() => {
+            navigate("/login");
+          });
         }
-    }, [user, logoutUser, navigate]);
 
-    return axiosSecure;
+        return Promise.reject(err);
+      }
+    );
+
+    return () => {
+      axiosSecure.interceptors.request.eject(reqInterceptors);
+      axiosSecure.interceptors.response.eject(resInterceptors);
+    };
+  }, [user, logoutUser, navigate]);
+
+  return axiosSecure;
 };
 
 export default useAxiosSecure;
